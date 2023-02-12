15709e0beb2d7937331afd3990659fbb888cd5a5
Sanjeev Mishra <mishragsanjeev1999@gmail.com>
2023-02-12 13:52:56 +0530

    elastic search posts api creaton done

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const elasticsearch = require("elasticsearch")
+const elasticClient = require("./elastic-client");
+require("express-async-errors");
 const app = express()
 app.use(bodyParser.json())
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("connected")
 })
+app.post("/create-post", async (req, res) => {
+    const result = await elasticClient.index({
+      index: "posts",
+      document: {
+        title: req.body.title,
+        author: req.body.author,
+        content: req.body.content,
+      },
+    });
+  
+    res.json(result);
+  });
